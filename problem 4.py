9fc9f8fa728c716daf0832b34f724dc353c24d7f
Shrekinator19 <43785257+Shrekinator19@users.noreply.github.com>
2018-12-18 10:07:49 -0500

    Update problem 4.py.

--- problem 4.py
+++ problem 4.py
@@ -1,4 +1,4 @@
-print('_' * 60)
+print('-'*60)
 print('Bouncer Bot:')
 print()
 age = input('What is your age today?')
@@ -6,4 +6,4 @@ if age == '24' :
 	print('Cool, come on in.')
 else:
 	print('Your going to need to back up. This club is 21+ only so find somewhere else to party or find out what robot punches feel like.')
-print('_' * 60)
+print('-'*60)
