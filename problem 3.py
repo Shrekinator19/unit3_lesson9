397bf261866582d66727b94b422d2c772d705c56
Shrekinator19 <43785257+Shrekinator19@users.noreply.github.com>
2018-12-18 10:07:17 -0500

    Update problem 3.py.

--- problem 3.py
+++ problem 3.py
@@ -1,4 +1,4 @@
-print('_' * 60)
+print('-'*60)
 print('Alien Greeter Bot:')
 print()
 planet = input('What planet are you from?')
@@ -6,4 +6,4 @@ if planet == 'Mars' :
 	print('Welcome to Earth, please do not kill us!')
 else:
 	print('Dammmmnnnnn, your face is so ugly I thought you were an alien. Anyway...have a good day!')
-print('_' * 60)
+print('-'*60)
